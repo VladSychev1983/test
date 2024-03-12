d855db5632c6394ca058194ec5f9a037cbd0b737
Vladislav Sychev <vladsychev1983@gmail.com>
2024-03-12 13:46:49 +0300

    Modified Task 8 solution

--- task_8.py
+++ task_8.py
@@ -2,17 +2,17 @@ import json
 import xml.etree.ElementTree as ET
 from pprint import pprint
 
-parse_files = ["newsafr.json","newsafr.xml"]
+parse_files = ["newsafr2.json","newsafr.xml"]
 
 def top_10(words_list,file):
     result_dict = {}
     counter = 0
-    for word in words_list:
-        if word in words_list and len(word) > 6 and word.isnumeric() == False:
-            counter += 1
-            result_dict[word] = counter
-        else:
-            result_dict[word] = 1     
+    unique_words  = [x for x in list(set(words_list)) if len(x)>6]
+    for uword in unique_words:
+        for tword in words_list:
+            if uword == tword:
+                result_dict.setdefault(uword,0)
+                result_dict[uword] += 1     
     result_dict_sorted = dict(sorted([(k,v) for k, v in result_dict.items()],reverse=True, key=lambda x: x[1])[:10])
     print(f'Топ-10 встречающихся слов в {file}:')
     for word,quantity in result_dict_sorted.items():
