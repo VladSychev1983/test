129ef3896c7934f2d77133dc3ce15d3825d453d1
Vladislav Sychev <vladsychev1983@gmail.com>
2024-05-19 22:29:02 +0300

    changed publisher.py

--- publisher.py
+++ publisher.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from publisher_models import create_tables, Publisher, Book, Stock, Shop, Sale
 from sqlalchemy.orm import aliased
 
-DSN = 'postgresql://postgres:password@192.168.32.215:5432/publisherdb'
+DSN = 'postgresql://postgres:juehtw@192.168.32.215:5432/publisherdb'
 engine = sqlalchemy.create_engine(DSN)
 
 create_tables(engine)
@@ -17,24 +17,24 @@ def get_shops (session, publisher_name):
     s = aliased(Stock)
     sl = aliased(Sale)
 
+    myquery = ''
     if publisher_name.isdigit():
         myquery = session.query(b, p, s, sl).\
         join(p, b.id_publisher == p.id).\
         join(s, b.id == s.id_book ).\
         join(sl, s.id == sl.id_stock).\
         filter(p.id == publisher_name).\
-        order_by(sl.date_sale).all()
-        for b,p,s,sl in myquery:
-            print(b.title, p.name, sl.price, sl.date_sale, sep=' | ',end='\n') 
+        order_by(sl.date_sale).all() 
     else:    
-        myquery = session.query(b, p, s, sl).\
+         myquery = session.query(b, p, s, sl).\
         join(p, b.id_publisher == p.id).\
         join(s, b.id == s.id_book ).\
         join(sl, s.id == sl.id_stock).\
         filter(p.name.like('%'+ publisher_name + '%')).\
         order_by(sl.date_sale).all()
-        for b,p,s,sl in myquery:
-            print(b.title, p.name, sl.price, sl.date_sale, sep=' | ',end='\n')    
+    
+    for b,p,s,sl in myquery:
+        print(b.title, p.name, sl.price, sl.date_sale, sep=' | ',end='\n')    
     session.close()
 
 if __name__ == "__main__":
