8859714330778552ec0b5d17af0c933b6bcd17c0
Vladislav Sychev <vladsychev1983@gmail.com>
2024-08-05 15:00:23 +0300

    Added django project

---
+++ django/django_netology/main/views.py
@@ -0,0 +1,9 @@
+import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse('hello from django!')
+
+def time(request):
+    return HttpResponse(f'Time = {datetime.datetime.now().time()}')
