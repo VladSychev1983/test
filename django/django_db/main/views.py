9116663eff7c32224159670509b29abb5a98f381
Vladislav Sychev <vladsychev1983@gmail.com>
2024-08-09 18:09:50 +0300

    django_db resolved sort by name

--- django/django_db/main/views.py
+++ django/django_db/main/views.py
@@ -4,10 +4,13 @@ from main.models import Phones
 
 def show_catalog(request):
     template = 'catalog.html'
+    sort_params = ['name','min_price','max_price']
+    sort_ = request.GET.get("sort")
     phones_obj = Phones.objects.all()
     phones_items = {}
     for p in phones_obj:
-        phones_items[p.id] = { 
+        phones_items[p.id] = {
+            'id': p.id, 
             'name' : p.name,
             'price' : p.price,
             'image' : p.image,
@@ -15,7 +18,28 @@ def show_catalog(request):
             'release_date': p.release_date,
             'slug' : p.slug,
         }
+    if sort_:
+        if sort_params.index(sort_) == 0:
+            print(f'Sorting by name..')
+            sorted_list = []
+            data_list = list(phones_items.values())
+            sorted_names = sorted([x["name"] for x in data_list])
+            for name in sorted_names:
+                for x in data_list:
+                    if name == x["name"]:
+                        sorted_list.append(x)
+            phones_items.clear()
+            for x in sorted_list:
+                phones_items[x["id"]] = x
+    
+        elif sort_params.index(sort_) == 1:
+            print(f'Sorting by min_price..')
+        
+        else:
+            print(f'Sorting by max_price..')
+
     context = {
+        'link_catalog': '/catalog',
         'phones' : phones_items
     }
     return render(request, template, context)
