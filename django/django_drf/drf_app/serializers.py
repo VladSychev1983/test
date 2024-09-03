9cb32babfd52cda46a20b2826eff6eb0b76ff513
Vladislav Sychev <vladsychev1983@gmail.com>
2024-09-03 11:27:50 +0300

    Added django_drf example project

---
+++ django/django_drf/drf_app/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from drf_app.models import Weapon
+
+#Ручной сереализатор. 
+# class WeaponSerializer(serializers.Serializer):
+#     power = serializers.IntegerField()
+#     rarity = serializers.CharField()
+
+#встроенный в django
+class WeaponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Weapon
+        fields = ['id','power','rarity']
+
