0e9bedfd91b9334bd5b708eceba66641abbc8f34
Vladislav Sychev <vladsychev1983@gmail.com>
2025-02-07 14:22:08 +0300

    Changed webpack folder.

--- javascript/webpack/src/index.js
+++ javascript/webpack/src/index.js
@@ -1,3 +1,6 @@
+import moment from 'moment';
+import moment from 'moment';
 import result from './data.txt';
 import './style.css';
 console.log(result)
+console.log(moment.now())
--- javascript/webpack/webpack.config.js
+++ javascript/webpack/webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+                },
+            {
             test: /\.txt$/,
             loader: 'raw-loader'
             },
